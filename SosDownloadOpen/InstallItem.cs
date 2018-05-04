e780c49473ac204d6eda539dfc81cdcf3f17b68c
Yukai Li <yukaili.geek@gmail.com>
2018-05-03 18:12:46 -0600

    Add single-EXE version of opener

---
+++ SosDownloadOpen/InstallItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Newtonsoft.Json;
+
+namespace LibSosHub
+{
+    public class InstallItem
+    {
+        public string Name { get; set; }
+        public string DownloadUrl { get; set; }
+        public string Description { get; set; }
+        public string Sponsor { get; set; }
+        public ContentType ContentType { get; set; }
+        public int ContentVersion { get; set; }
+        public string AuxData { get; set; }
+        public string CommandLineArgs { get; set; }
+        public string Reserved { get; set; }
+        //[JsonIgnore]
+        public byte[] Payload { get; set; }
+        //[JsonIgnore]
+        public byte[] SponsorBanner { get; set; }
+        public string PayloadName { get; set; }
+        public string SponsorBannerName { get; set; }
+    }
+}
